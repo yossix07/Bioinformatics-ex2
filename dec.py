735df4400c3fa46045fe2699302b3751bb8b2cca
NoamGini <noamgini10@gmail.com>
2023-05-27 14:06:17 +0300

    handling local max

--- dec.py
+++ dec.py
@@ -1,17 +1,20 @@
 import random
 
 # Global variables
+from matplotlib import pyplot as plt
+
 POPULATION_SIZE = 100
 MAX_GENERATIONS = 90
-MUTATION_RATE = 0.12
+MUTATION_RATE = 1
 REPLACEMENT_RATE = 0.15
 REPLACEMENT_SIZE = int(POPULATION_SIZE * REPLACEMENT_RATE)
 EPSILON = 0.0001
 NO_IMPROVEMENT_THRESHOLD = 12
-
+OPTIMAL = False
 DARWIN = False
 LAMARCK = False
 LOCAL_SWAPS = 5
+OPTIMAL_THRESHOLD = 0.98
 
 fitness_calls_coutner = 0
 
@@ -71,27 +74,35 @@ def calculate_letters_pairs_frequency_from_text(text):
 
 def calculate_fitness(decryption_key, ciphertext):
     global fitness_calls_coutner
+    global OPTIMAL
+    OPTIMAL = False
+    #optimal_solution=False
     fitness_calls_coutner += 1
-    decrypted_text = decrypt_text(ciphertext, decryption_key)
+    decrypted_text = decrypt_text(ciphertext, decryption_key).replace(".", "").replace(",", "").replace(";", "")
+
     decrypted_text_set = set(decrypted_text.split(" "))
     letter_freq = calculate_letter_frequency_from_text(decrypted_text)
     letter_pair_freq = calculate_letters_pairs_frequency_from_text(decrypted_text)
     words_matching = 0
     letters_matching = 0
 
-    for word in word_list:
-        if word == '':
-            continue
+    actual_word_list = [element for element in word_list if element != ""]
+    num_of_word_in_text = len(decrypted_text_set)
+
+    for word in actual_word_list:
         if word in decrypted_text_set:
             words_matching += 1
 
     for letter in letter_freq_map:
         if letter in letter_freq and letter in letter_freq_map:
             letters_matching += (1 - abs(letter_freq[letter] - letter_freq_map[letter])) ** 2
-
+#    print(letters_matching)
     for letter_pair in letter_pair_freq_map:
         if letter_pair in letter_pair_freq and letter_pair in letter_pair_freq_map:
             letters_matching += (1 - abs(letter_pair_freq[letter_pair] - letter_pair_freq_map[letter_pair])) ** 2.5
+
+    if words_matching >= OPTIMAL_THRESHOLD*num_of_word_in_text:
+        OPTIMAL = True
 
     return words_matching + letters_matching
 
@@ -105,6 +116,7 @@ def decrypt_text(ciphertext, decryption_key):
             decrypted_text += letter
     return decrypted_text
 
+
 def generate_random_key():
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     shuffled_alphabet = list(alphabet)
@@ -113,6 +125,7 @@ def generate_random_key():
     for i, letter in enumerate(alphabet):
         decryption_key[letter] = shuffled_alphabet[i]
     return decryption_key
+
 
 def crossover(parent1, parent2):
     keys = list(parent1.keys())
@@ -150,8 +163,6 @@ def mutate(decryption_key):
     return mutated_key
 
 
-import random
-
 def select_parents(population, fitness_scores, tournament_size=5):
     parents = []
     
@@ -162,7 +173,6 @@ def select_parents(population, fitness_scores, tournament_size=5):
         parents.append(population[winner_index])
     
     return parents
-
 
 
 def replace_population(population, offspring, fitness_scores):
@@ -196,6 +206,7 @@ def replace_population(population, offspring, fitness_scores):
 
     return population
 
+
 def local_optimization(individual, n=LOCAL_SWAPS):
     keys = list(individual.keys())
     for _ in range(n):
@@ -204,12 +215,14 @@ def local_optimization(individual, n=LOCAL_SWAPS):
         individual[letter1], individual[letter2] = individual[letter2], individual[letter1]
     return individual
 
+
 def darwin_mutation(population, fitness_scores, ciphertext):
     for i in range(len(population)):
         mutated_individual = mutate(population[i])
         mutated_fitness = calculate_fitness(mutated_individual, ciphertext)
         if mutated_fitness > fitness_scores[i]:
             fitness_scores[i] = mutated_fitness
+
 
 def lamarck_mutation(population, fitness_scores, ciphertext):
     for i in range(len(population)):
@@ -219,11 +232,44 @@ def lamarck_mutation(population, fitness_scores, ciphertext):
             fitness_scores[i] = mutated_fitness
             population[i] = mutated_individual
 
+
+def handle_local_max(ciphertext,best_decryption_key, best_decryption_key_fitness):
+    print("handling local max")
+    for i in range(5):
+        best_key, _, best_fitness = genetic_algorithm(ciphertext)
+        if best_decryption_key_fitness < best_fitness:
+            print(best_decryption_key_fitness)
+            print(best_fitness)
+            best_decryption_key_fitness = best_fitness
+            best_decryption_key = best_key
+        if OPTIMAL:
+            return best_decryption_key
+
+    print(best_decryption_key_fitness)
+    return best_decryption_key
+
+
+def generate_graph(main_text,y_text,num_generation, generation_scores ):
+    plt.plot(num_generation, generation_scores)
+    plt.title(
+        main_text)
+    plt.ylabel(y_text)
+    plt.xlabel('Number of generations')
+    plt.show()
+
+
 def genetic_algorithm(ciphertext):
+    global generations_average_fitness_scores
+    global generations_best_fitness_scores
+    global num_of_generations
+    generations_best_fitness_scores = []
+    generations_average_fitness_scores = []
     # Initialize random population
     population = [generate_random_key() for _ in range(POPULATION_SIZE)]
     no_improvement_counter = 0
     best_fitness = 0
+    gen_num = 0
+
     for generation in range(MAX_GENERATIONS):
         fitness_scores = []
         offspring = []
@@ -232,13 +278,19 @@ def genetic_algorithm(ciphertext):
         for decryption_key in population:
             fitness = calculate_fitness(decryption_key, ciphertext)
             fitness_scores.append(fitness)
+            if OPTIMAL:
+                print('optimal')
+                gen_num = generation
+                num_of_generations = [i for i in range(gen_num)]
+                return decryption_key, 0, fitness
 
         # Select parents and create offspring
         for _ in range(POPULATION_SIZE//2):
             parent1, parent2 = select_parents(population, fitness_scores)
             child = crossover(parent1, parent2)
-            mutated_child = mutate(child)
-            offspring.append(mutated_child)
+            if random.random() < MUTATION_RATE:
+                child = mutate(child)
+            offspring.append(child)
 
             
         # Replace population with offspring
@@ -254,6 +306,9 @@ def genetic_algorithm(ciphertext):
         best_index = fitness_scores.index(max(fitness_scores))
         best_key = population[best_index]
         best_fitness = fitness_scores[best_index]
+        average_gen_fitness = sum(fitness_scores) / len(fitness_scores)
+        generations_average_fitness_scores.append(average_gen_fitness)
+        generations_best_fitness_scores.append(best_fitness)
 
         if best_fitness - temp_best_fitness < EPSILON:
             no_improvement_counter += 1
@@ -262,12 +317,44 @@ def genetic_algorithm(ciphertext):
 
         if no_improvement_counter == NO_IMPROVEMENT_THRESHOLD:
             break
+
         print(f"Generation: {generation+1} | Best Fitness: {best_fitness} | Best Decryption Key: {best_key}")
 
-    return best_key
+        gen_num = generation+1
+    print(gen_num)
+    num_of_generations = [i for i in range(gen_num)]
+
+    return best_key, no_improvement_counter, best_fitness
+
+
 
 ciphertext = read_text_from_file('enc.txt')
-best_decryption_key = genetic_algorithm(ciphertext)
+
+#initialize lists to generate graphs
+num_of_generations = []
+generations_best_fitness_scores = []
+generations_average_fitness_scores = []
+
+#create average fitness score graph
+create_graph_avg = True
+
+#create best fitness score graph
+create_graph_best = True
+
+best_decryption_key, counter, fitness = genetic_algorithm(ciphertext)
+
+#handle local max
+if counter == NO_IMPROVEMENT_THRESHOLD and not OPTIMAL:
+    best_decryption_key = handle_local_max(ciphertext, best_decryption_key, fitness)
+
+#generate graphs
+if create_graph_avg:
+    generate_graph('Average fitness score','Average fitness score',num_of_generations,
+                   generations_average_fitness_scores)
+if create_graph_best:
+    generate_graph('Best fitness score', 'Best fitness score', num_of_generations,
+                   generations_best_fitness_scores)
+
 decrypted_text = decrypt_text(ciphertext, best_decryption_key)
 # print("Decrypted Text:", decrypted_text)
 
@@ -301,8 +388,16 @@ actual_decryption_key = {
 }
 
 correct = 0
+print(best_decryption_key)
 for letter in actual_decryption_key:
     if actual_decryption_key[letter] == best_decryption_key[letter]:
         correct += 1
 print("Correct:", correct, " out of", len(actual_decryption_key))
 print("fitness calls: ", fitness_calls_coutner)
+
+# creating files
+with open("plain.txt", "w") as plain_file:
+    plain_file.write(decrypted_text)
+with open("perm.txt", "w") as perm_file:
+    for letter, decrypted_letter in best_decryption_key.items():
+        perm_file.write(f"{letter}\t{decrypted_letter}\n")
